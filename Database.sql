1d0d49e992e0226291c784f0d0cd66e6d9a9d553
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-09-28 12:57:15 +0530

    Version2
    Final_Database_Blueprint_Created

--- Database.sql
+++ Database.sql
@@ -1,6 +1,26 @@
 create database demo;
 use demo;
 
+/*
+Entities
+-Account
+-Administration
+-Student
+-Professor
+-Department
+-Courses
+-Time_Slots
+-Study_Material
+
+Relationships
+-Admin_Account_Relation
+-Student_Account_Relation
+-Professor_Account_Relation
+-Courses_Student_Relation
+-Courses_Professor_Relation
+-Courses_Time_Slots_Relation
+*/
+
 create table Administration(
 Admin_ID varchar(30),
 primary key(Admin_ID)
@@ -16,6 +36,22 @@ primary key(Username)
 );
 /*drop table Account*/
 
+create table Admin_Account_Relation(
+Admin_ID varchar(30),
+Username varchar(30),
+primary key(Username),
+foreign key(Admin_ID) references Administration(Admin_ID),
+foreign key(Username) references Account(Username)
+);
+/*drop table Admin_Account_Relation*/
+
+create table Department(
+Dept_ID int,
+D_Name varchar(40) not null,
+primary key(Dept_ID)
+);
+/*drop table Department;*/
+
 create table Student(
 Roll_No int,
 S_Name varchar(40),
@@ -28,13 +64,6 @@ foreign key(Department_ID) references Department(Dept_ID)
 );
 /*drop table Student*/
 
-create table Department(
-Dept_ID int,
-D_Name varchar(40) not null,
-primary key(Dept_ID)
-);
-/*drop table Department;*/
-
 create table Professor(
 Employee_ID int,
 P_Name varchar(40) not null,
@@ -45,6 +74,24 @@ foreign key(Department_ID) references Department(Dept_ID)
 );
 /*drop table Professor*/
 
+create table Student_Account_Relation(
+Roll_No int,
+Username varchar(30),
+primary key(Username),
+foreign key(Roll_No) references Student(Roll_No),
+foreign key(Username) references Account(Username)
+);
+/*drop table Student_Account_Relation*/
+
+create table Professor_Account_Relation(
+Employee_ID int,
+Username varchar(30),
+primary key(Username),
+foreign key(Employee_ID) references Professor(Employee_ID),
+foreign key(Username) references Account(Username)
+);
+/*drop table Professor_Account_Relation*/
+
 create table Courses(
 Course_Code varchar(7),
 Course_Name varchar(50) not null,
@@ -54,17 +101,47 @@ primary key(Course_Code)
 );
 /*drop table Courses*/
 
+create table Courses_Student_Relation(
+Course_Code varchar(7),
+Roll_No int,
+Attendance int,
+check(Attendance<=100 and Attendance>=0),
+primary key(Course_Code),
+foreign key(Roll_no) references Student(Roll_No),
+foreign key(Course_Code) references Courses(Course_Code)
+);
+/*drop table Courses_Student_Relation*/
+
+create table Courses_Professor_Relation(
+Course_Code varchar(7),
+Employee_ID int,
+primary key(Course_Code),
+foreign key(Employee_ID) references Professor(Employee_ID),
+foreign key(Course_Code) references Courses(Course_Code)
+);
+/*drop table Courses_Professor_Relation*/
+
 create table Time_Slots(
-Day_ varchar(10),
-Time_ varchar(10),
-primary key(Day_,Time_)
+Day_Time varchar(20),
+primary key(Day_Time)
 );
 /*drop table Time_Slots*/
+
+create table Courses_Time_Slots_Relation(
+Course_Code varchar(7),
+Day_Time varchar(20),
+primary key(Day_Time,Course_Code),
+foreign key(Day_Time) references Time_Slots(Day_Time),
+foreign key(Course_Code) references Courses(Course_Code)
+);
+/*drop table Courses_Time_Slots_Relation*/
 
 create table Study_Material(
 Material_No int,
 Link varchar(200),
-primary key(Material_No)
+Course_Code varchar(7),
+primary key(Material_No),
+foreign key(Course_Code) references Courses(Course_Code)
 );
 /*drop table Study_Material*/
 
