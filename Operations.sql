f460fa13c9b585b36d9bbdfe6fec7db15a70e6ce
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-10-07 22:27:45 +0530

    Version4
    -Almost completed

--- Operations.sql
+++ Operations.sql
@@ -232,6 +232,262 @@ delimiter ;
 call Delete_Professor(10);  /* Employee ID */
 /*End*/
 
- 
+/*Insert Course*/
+delimiter //
+create procedure Insert_Course(
+in code varchar(7),
+in name varchar(50),
+in link varchar(200),
+in credit varchar(5),
+out did int
+)
+begin
+declare exit handler for 1062
+begin
+set did=1;
+end;
+insert into Courses values(code, name, link, credit); 
+end //
+delimiter ;  
+/* Execute */
+call Insert_Course('CS 207','Database','xyz.com','3-0-0',@did); /* Course Code, Course Name, Class link, Credits */
+select @did;                                                    /* Duplicate ID problem */
+/* End */
+
+/*Delete Course*/
+delimiter //
+create procedure Delete_Course(
+in code varchar(7),
+out rif int
+)
+begin
+declare exit handler for 1451
+begin
+set rif=1;
+end;
+delete from Courses where Courses.Course_Code=code;
+end //
+delimiter ;
+/*Execute*/
+call Delete_Course('CS 207',@rif);   /* Course Code */
+select @rif;                         /* Referential Integrity failure( In case any student or professor associated with course ) */
+/*End*/
+
+/* Insert Student's Course */
+delimiter //
+create procedure Add_Student_Course(
+in code varchar(7),
+in rollno int,
+in total int,
+in attend int,
+out did int,
+out rif int,
+out inv int
+)
+begin
+declare exit handler for 1062
+begin
+set did=1;
+end;
+declare exit handler for 1452
+begin
+set rif=1;
+end;
+declare exit handler for 3819
+begin
+set inv=1;
+end;
+insert into Courses_Student_Relation values(code,rollno,total,attend);
+end //
+delimiter ;
+/* Execute */
+call Add_Student_Course('CS 207',1,100,98,@did,@rif,@inv); /* Course Code, Roll No., Total class days, Classes attended */
+select @did;                                                /* Duplicate ID problem */
+select @rif;                                                /* Referential Integrity Problem( Student or Course DNE ) */
+select @inv;                                                /* Invalid entry of attendance */
+/*End*/
+
+/* Delete Student's Course */
+delimiter //
+create procedure Remove_Student_Course(
+in rollno int,
+in code varchar(7)
+)
+begin
+delete from Courses_Student_Relation where Courses_Student_Relation.Roll_No=rollno and Courses_Student_Relation.Course_Code=code;
+end //
+delimiter ;
+/* Execute */
+call Remove_Student_Course(1,'CS 207'); /* Roll No, Course Code */
+/*End*/
+
+/* Insert Professor's Course */
+delimiter //
+create procedure Add_Professor_Course(
+in code varchar(7),
+in empid int,
+out did int,
+out rif int
+)
+begin
+declare exit handler for 1062
+begin
+set did=1;
+end;
+declare exit handler for 1452
+begin
+set rif=1;
+end;
+insert into Courses_Professor_Relation values(code,empid);
+end //
+delimiter ;
+/* Execute */
+call Add_Professor_Course('CS 207',20,@did,@rif);           /* Course Code, Employee ID */
+select @did;                                                /* Duplicate ID problem */
+select @rif;                                                /* Referential Integrity Problem( Professor or Course DNE ) */
+/*End*/
+
+/* Delete Professor's Course */
+delimiter //
+create procedure Remove_Professor_Course(
+in empid int,
+in code varchar(7)
+)
+begin
+delete from Courses_Professor_Relation where Courses_Professor_Relation.Employee_ID=empid and Courses_Professor_Relation.Course_Code=code;
+end //
+delimiter ;
+/* Execute */
+call Remove_Professor_Course(20,'CS 207'); /* Employee ID, Course Code */
+/*End*/
+
+/*Insert TimeSlot*/
+delimiter //
+create procedure Insert_Time_Slot(
+in day varchar(20),
+in time varchar(5),
+out did int
+)
+begin
+declare exit handler for 1062
+begin
+set did=1;
+end;
+insert into Time_Slots values(day,time);
+end //
+delimiter ;
+/*Execute*/
+call Insert_Time_Slot('Monday','17:00',@did); /* Day, Time */
+select @did;                                  /* Time Slot already exists */
+/* End */
+
+/*Insert Study Material */
+delimiter //
+create procedure Insert_Study_Material(
+in link varchar(200),
+in code varchar(7),
+out rif int
+)
+begin
+declare exit handler for 1452
+begin
+set rif=1;
+end;
+insert into Study_Material(Link, Course_Code) values(link,code);
+end //
+delimiter ;
+/*Execute*/
+call Insert_Study_Material('xyz.com','CS 207',@rif); /* Link, Course Code */
+select @rif;                                         /* Referential Integrity Failure(Course DNE)*/ 
+/*End*/
+
+/*Delete Study Material */
+delimiter //
+create procedure Delete_Study_Material(
+in code varchar(7),
+in link varchar(200)
+)
+begin
+delete from Study_Material where Study_Material.Course_Code=code and Study_Material.Link=link;
+end //
+delimiter ;
+/*Execute*/
+call Delete_Study_Material('CS 207','xyz.com'); /* Course Code, Link */
+/*End*/
+
+/* Assign Time Slot */
+delimiter //
+create procedure Assign_Time_Slot(
+in code varchar(7),
+in day varchar(20),
+in time varchar(5),
+out rif int
+)
+begin
+declare exit handler for 1452
+begin
+set rif=1;
+end;
+insert into Courses_Time_Slots_Relation values(code,day,time);
+end //
+delimiter ;
+/*Execute*/
+call Assign_Time_Slot('CS 207','Monday','17:00',@rif);
+select @rif;
+/*End*/
+
+/* Unassign Time Slot*/
+delimiter //
+create procedure Unassign_Time_Slot(
+in code varchar(7),
+in day varchar(20),
+in time varchar(5)
+)
+begin
+delete from Courses_Time_Slots_Relation as a where a.Course_Code=code and a.Day=day and a.Time=time; 
+end //
+delimiter ;
+/*Execute*/
+call Unassign_Time_Slot('CS 207','Monday','17:00');
+/*End*/
+
+/* Get Student Time Table */
+delimiter //
+create procedure Student_Time_Table(
+in rollno int
+)
+begin
+select a.Course_Code, b.Day, b.Time from 
+Courses_Student_Relation as a join Courses_Time_Slots_Relation as b
+on a.Course_Code=b.Course_Code
+where a.Roll_No=rollno;
+end //
+delimiter ;
+/*Execute*/
+call Student_Time_Table(1); /* Roll no. */
+/*End*/
+
+/* Get Professor Time Table */
+delimiter //
+create procedure Professor_Time_Table(
+in empid int
+)
+begin
+
+
+
+select a.Course_Code, b.Day, b.Time from 
+Courses_Professor_Relation as a join Courses_Time_Slots_Relation as b
+on a.Course_Code=b.Course_Code
+where a.Employee_ID=empid
+order by b.Day, b.Time ASC;
+end //
+delimiter ;
+/*Execute*/
+call Professor_Time_Table(1); /* Employee ID */
+/*End*/
+
+
+
 
 
