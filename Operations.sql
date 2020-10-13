d36345dad2ce79bdd891d94c0478074bbbbb7ae5
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-10-13 22:18:15 +0530

    Version8
    -Added procedure Retrieve_Study_Material

--- Operations.sql
+++ Operations.sql
@@ -952,3 +952,18 @@ delimiter ;
 /*Execute*/
 call Attendance_Today('CS 207','Monday'); /*Course Code, Day */
 /*End*/
+
+/*Retrieve Study Material*/
+delimiter //
+create procedure Retrieve_Study_Material(
+in rollno int
+)
+begin
+select a.Course_Code, a.Link
+from Study_Material as a
+where a.Course_Code in (select b.Course_Code from Courses_Student_Relation as b where b.Roll_No=rollno );
+end //
+delimiter ;
+/*Execute*/
+call Retrieve_Study_Material(1); /*Roll No.*/
+/*End*/
