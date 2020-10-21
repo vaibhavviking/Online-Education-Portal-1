633dbb028e40067cbc5d2e969ae2e8fb3f6a407d
vaibhav <chandravaibhav65@gmail.com>
2020-10-21 21:06:53 +0530

    session id

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const cookieParser = require('cookie-parser');
 // const Stratergy = require('passport-local').Stratergy;
 
 
-var port = process.env.PORT || 6666;
+
 
 var app = express();
 app.use(cookieParser());
@@ -35,14 +35,22 @@ app.use((req, res, next) => {
     next();
 });
 
-var connection = mysql.createConnection({
+var db_config = {
     multipleStatements: true,
     host: 'us-cdbr-east-02.cleardb.com',
     user: 'be25eca42b63ff',
     password: '74074572',
     database: 'heroku_ba6bdaf56728c3e',
     // port: 3306
-});
+};
+// var db_config = {
+//     multipleStatements: true,
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'password',
+//     database: 'demo',
+//     port: 3306
+// };
 
 // var connection = mysql.createConnection({
 //     multipleStatements: true,
@@ -53,15 +61,43 @@ var connection = mysql.createConnection({
 //     port: 3306
 // });
 
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
 
-connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
 
-    console.log('connected as id ' + connection.threadId);
-});
+handleDisconnect();
+
+var port = process.env.PORT || 5000;
+
+
+
+
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+
+//     console.log('connected as id ' + connection.threadId);
+// });
 
 app.set('view engine', 'ejs');
 
@@ -572,7 +608,7 @@ app.get('/prof_courses', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log("application started successfully")
+    console.log("application started successfully",port)
 })
 
 app.get('/ongoing_classes', (req, res) => {
@@ -651,7 +687,7 @@ async function student_authenticate(username, password, res, req) {
                     console.log('cache cleared');
                 })
                 var query2 = 'INSERT INTO current_session VALUES (?,?)';
-                connection.query(query2, [req.sessionID, result[1][0]['@ID']], (err, results1) => {
+                connection.query(query2, [req.session.user, result[1][0]['@ID']], (err, results1) => {
                     if (err) throw err;
                     console.log(result[1][0]['@ID']);
                     console.log('Instance created');
@@ -685,7 +721,7 @@ async function teacher_authenticate(username, password, res, req) {
                     console.log('cache cleared');
                 })
                 var query2 = 'INSERT INTO current_session VALUES (?,?)';
-                connection.query(query2, [req.sessionID, result[1][0]['@ID']], (err, results1) => {
+                connection.query(query2, [req.session.user, result[1][0]['@ID']], (err, results1) => {
                     if (err) throw err;
                     console.log(result[1][0]['@ID']);
                     console.log('Instance created');
@@ -718,7 +754,7 @@ async function admin_authenticate(username, password, res, req) {
                     console.log('cache cleared');
                 })
                 var query2 = 'INSERT INTO current_session VALUES (?,?)';
-                connection.query(query2, [req.sessionID, result[1][0]['@ID']], (err, results1) => {
+                connection.query(query2, [req.session.user, result[1][0]['@ID']], (err, results1) => {
                     if (err) throw err;
                     console.log(result[1][0]['@ID']);
                     console.log('Instance created');
