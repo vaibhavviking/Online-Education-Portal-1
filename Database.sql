86797cc66c362fbf73852dfb1a15874a530b38a2
vaibhav <chandravaibhav65@gmail.com>
2020-10-21 15:44:30 +0530

    Database changed 4

--- Database.sql
+++ Database.sql
@@ -150,7 +150,7 @@ foreign key(Course_Code) references Courses(Course_Code) on delete cascade
 
 create table current_session(
 Session_ID varchar(50),
-User_ID_ varchar(30),
+User_ID varchar(30),
 primary key(User_ID_),
 foreign key(User_ID_) references Account(User_ID_) on delete cascade
 );
