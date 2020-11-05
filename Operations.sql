6247bed52e5bf0fdce8f96caf3c103a5d5641da6
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-11-05 13:28:55 +0530

    Version11
    -Change Password procedure edited

--- Operations.sql
+++ Operations.sql
@@ -82,14 +82,22 @@ delimiter //
 create procedure Change_Password(
 in userid varchar(30),
 in old_p varchar(30),
-in new_p varchar(30)
+in new_p varchar(30),
+out matched int
 )
 begin
+declare p int;
+select Account.Password_ into p from Account where User_ID_=userid;
+case
+	when p=old_p then set matched=1;
+    else set matched=0;
+end case;
 update Account set Password_=new_p where Account.User_ID_=userid and Account.Password_=old_p;
 end //
 delimiter ;
 /*Execute*/
-call Change_Password('A2','b1','b'); /* User_ID, Current Password, New Password */
+call Change_Password('A2','b1','b',@m); /* User_ID, Current Password, New Password */
+select @m;
 /*End*/
 
 /*Insert Department*/
