aa7db56792abc8807520232479667cc0cb14df7b
vaibhav chandra <56549454+vaibhavviking@users.noreply.github.com>
2020-11-08 00:35:30 +0530

    Update Total_Days_Increment.sql

--- Database/Procedures/Total_Days_Increment.sql
+++ Database/Procedures/Total_Days_Increment.sql
@@ -1,9 +1,12 @@
 /*Total Days Incrementer*/
 delimiter //
-create procedure Total_Days_Increment()
+create procedure Total_Days_Increment(
+    in course varchar(7)
+)
 begin
 update Courses_Student_Relation as a
-set a.Total_Days=a.Total_Days+1;
+set a.Total_Days=a.Total_Days+1
+where a.Course_Code=course;
 end //
 delimiter ;
 /*Execute*/
