4f926e74f4b4856d3e57b3acc3b065092761ca62
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-11-07 16:42:47 +0530

    Get study materials of courses taught by professor

--- Database/Procedures/Retrieve_Professor_Study_Material.sql
+++ Database/Procedures/Retrieve_Professor_Study_Material.sql
@@ -4,7 +4,7 @@ create procedure Retrieve_Professor_Study_Material(
 in empid int
 )
 begin
-select a.Course_Code, a.Link
+select a.Material_No, a.Course_Code, a.Link
 from Study_Material as a
 where a.Course_Code in (select b.Course_Code from Courses_Professor_Relation as b where b.Employee_ID=empid );
 end //
