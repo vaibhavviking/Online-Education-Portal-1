98cf5ac0cb0a3ce8abdd786991477eceb626ee80
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-11-14 01:21:52 +0530

    Get Email from User ID

--- Database/Procedures/Retrieve_Email.sql
+++ Database/Procedures/Retrieve_Email.sql
@@ -9,19 +9,19 @@ declare t varchar(15);
 select Type_ into t from Account where User_ID_=u;
 case
 	when t='Admin' then 
-    select Email into email from Account inner join Admin_Account_Relation 
-    on Account.User_ID_=Admin_Account_Relation.User_ID_ 
-    where Account.User_ID_=u;
+    select Email into email from Administration inner join Admin_Account_Relation 
+    on Administration.Admin_ID=Admin_Account_Relation.Admin_ID 
+    where Admin_Account_Relation.User_ID_=u;
     
     when t='Student' then 
-    select Email into email from Account inner join Student_Account_Relation 
-    on Account.User_ID_=Student_Account_Relation.User_ID_ 
-    where Account.User_ID_=u;
+    select Email into email from Student inner join Student_Account_Relation 
+    on Student.Roll_No=Student_Account_Relation.Roll_No 
+    where Student_Account_Relation.User_ID_=u;
     
     when t='Professor' then 
-    select Email into email from Account inner join Professor_Account_Relation 
-    on Account.User_ID_=Professor_Account_Relation.User_ID_ 
-    where Account.User_ID_=u;
+    select Email into email from Professor inner join Professor_Account_Relation 
+    on Professor.Employee_ID=Professor_Account_Relation.Employee_ID 
+    where Professor_Account_Relation.User_ID_=u;
     
     else set email='NOT FOUND';
 end case;
