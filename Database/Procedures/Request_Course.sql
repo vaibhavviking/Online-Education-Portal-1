fbca407a8e615ff8ea8b774a967489e581944c4a
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-11-14 18:30:53 +0530

    Request Access to course

--- Database/Procedures/Request_Course.sql
+++ Database/Procedures/Request_Course.sql
@@ -2,12 +2,18 @@
 delimiter //
 create procedure Request_Course(
 in rollno int,
-in code varchar(7)
+in code varchar(7),
+out did int
 )
 begin
+declare exit handler for 1062
+begin
+set did=1;
+end;
 insert into Requested_Courses values(rollno, code);
 end //
 delimiter ;
 /*Execute*/
-call Request_Course(1,'CS 207'); /* Roll No, Course_Code */
+call Request_Course(1,'CS 207',@did); /* Roll No, Course_Code */
+select @did;                          /* Duplicate record */
 /*End*/
