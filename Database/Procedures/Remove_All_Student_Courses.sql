362ed5efdb1674503a4c8ecff73f881ed1045fdf
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-11-16 00:57:04 +0530

    Unassign all courses a student is enrolled in

--- Database/Procedures/Remove_All_Student_Courses.sql
+++ Database/Procedures/Remove_All_Student_Courses.sql
@@ -4,9 +4,9 @@ create procedure Remove_All_Student_Courses(
 in rollno int
 )
 begin
-delete from Courses_Student_Relation where Courses_Student_Relation.Roll_No=rollno; 
 update Course_Wise_Members set No_Of_Students=No_Of_Students-1 where Course_Wise_Members.CID in 
 (select Courses_Student_Relation.Course_Code from Courses_Student_Relation where Courses_Student_Relation.Roll_No=rollno);
+delete from Courses_Student_Relation where Courses_Student_Relation.Roll_No=rollno; 
 end //
 delimiter ;
 /*Execute*/
