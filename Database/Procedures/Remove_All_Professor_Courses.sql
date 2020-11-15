0f929d16e5575476b111649c2d385b8f2c1d88eb
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-11-16 00:34:20 +0530

    Unassign all courses taught by professor

--- Database/Procedures/Remove_All_Professor_Courses.sql
+++ Database/Procedures/Remove_All_Professor_Courses.sql
@@ -5,8 +5,11 @@ in empid int
 )
 begin
 delete from Courses_Professor_Relation where Courses_Professor_Relation.Employee_ID=empid; 
+update Course_Wise_Members set No_Of_Professors=No_Of_Professors-1 where Course_Wise_Members.CID in 
+(select Courses_Professor_Relation.Course_Code from Courses_Professor_Relation where Courses_Professor_Relation.Employee_ID=empid);
 end //
 delimiter ;
 /*Execute*/
 call Remove_All_Professor_Courses(1); /*Employee ID*/
 /*End*/
+drop procedure Remove_All_Professor_Courses;
