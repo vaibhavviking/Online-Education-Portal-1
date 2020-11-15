5d6f5b14d83e18f90d4c7aa27dbdd68ff18f1930
ChakPC <64187763+ChakPC@users.noreply.github.com>
2020-11-16 00:58:32 +0530

    Unassign a course of student

--- Database/Procedures/Remove_Student_Course.sql
+++ Database/Procedures/Remove_Student_Course.sql
@@ -6,8 +6,11 @@ in code varchar(7)
 )
 begin
 delete from Courses_Student_Relation where Courses_Student_Relation.Roll_No=rollno and Courses_Student_Relation.Course_Code=code;
+update Course_Wise_Members set No_Of_Students=No_Of_Students-1 where Course_Wise_Members.CID=code;
 end //
 delimiter ;
 /* Execute */
-call Remove_Student_Course(1,'CS 207'); /* Roll No, Course Code */
+call Remove_Student_Course(1,'MA 205'); /* Roll No, Course Code */
 /*End*/
+drop procedure Remove_Student_Course;
+
